45068b55133058347f9181e9eae07bbebbe3f86b
HenokGuluma <htg218@nyu.edu>
2021-04-19 15:53:37 +0300

    Clean up config files

---
+++ src/app/(app)/customers/[id]/page.tsx
@@ -0,0 +1,31 @@
+import { prisma } from "@/lib/prisma";
+import { getSessionUser } from "@/lib/auth";
+import { redirect, notFound } from "next/navigation";
+import { CustomerDetailView } from "@/components/CustomerDetailView";
+import { demoScope } from "@/lib/demo-scope";
+import { fullName } from "@/lib/format";
+import type { CustomerWithDetail } from "@/types";
+
+export default async function CustomerDetailPage({ params }: { params: { id: string } }) {
+  const user = await getSessionUser();
+  if (!user) redirect("/login");
+
+  const customer = await prisma.customer.findUnique({
+    where: { id: params.id, ...demoScope(user) },
+    include: {
+      contacts: { orderBy: [{ isPrimary: "desc" }, { createdAt: "asc" }] },
+      documents: { orderBy: { uploadedAt: "desc" }, include: { uploadedBy: { select: { id: true, firstName: true, lastName: true } } } },
+    },
+  });
+  if (!customer) notFound();
+
+  const withComposedNames = {
+    ...customer,
+    documents: customer.documents.map((d) => ({
+      ...d,
+      uploadedBy: d.uploadedBy ? { id: d.uploadedBy.id, name: fullName(d.uploadedBy.firstName, d.uploadedBy.lastName) } : null,
+    })),
+  };
+
+  return <CustomerDetailView user={user} initialCustomer={JSON.parse(JSON.stringify(withComposedNames)) as CustomerWithDetail} />;
+}
