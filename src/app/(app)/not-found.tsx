6d44acdbe9fb2ceb9ce3e3e97f82f3f34a0a455f
HenokGuluma <htg218@nyu.edu>
2021-08-24 13:51:18 +0300

    Add 2 files module

---
+++ src/app/(app)/not-found.tsx
@@ -0,0 +1,14 @@
+import { Icon, Button } from "@/components/ui";
+
+export default function AppNotFound() {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", padding: "80px 20px", textAlign: "center" }}>
+      <Icon name="package" size={34} style={{ color: "var(--muted)", marginBottom: 14 }} />
+      <h2 style={{ margin: "0 0 6px 0", fontFamily: "var(--font-heading)" }}>Not found</h2>
+      <p style={{ color: "var(--muted)", fontSize: 13.5, maxWidth: 420, marginBottom: 20 }}>
+        This record doesn&apos;t exist — it may have been deleted, or the link is out of date.
+      </p>
+      <a href="/dashboard" style={{ textDecoration: "none" }}><Button variant="primary">Back to Dashboard</Button></a>
+    </div>
+  );
+}
