3cf6488ddaa3eeeb8e333763a83d15a5db786c94
HenokGuluma <htg218@nyu.edu>
2021-09-20 10:25:57 +0300

    Clean up layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,59 @@
+import type { Metadata, Viewport } from "next";
+import { Inter, Manrope, JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  weight: ["400", "500", "600", "700", "800"],
+});
+
+// Bold, standard, modern — used for headings, nav, KPI values, and anywhere
+// that should read as confidently "designed" without being quirky.
+const manrope = Manrope({
+  subsets: ["latin"],
+  variable: "--font-heading",
+  weight: ["600", "700", "800"],
+});
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+  weight: ["400", "500", "600"],
+});
+
+export const metadata: Metadata = {
+  title: "Edget · Dispatch",
+  description: "Internal freight dispatching platform",
+  icons: {
+    icon:
+      "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Crect width='100' height='100' rx='22' fill='%23131B2E'/%3E%3Cpath d='M20 62h8V40h20l10 12v10h8' stroke='%23F5A623' stroke-width='6' fill='none' stroke-linecap='round' stroke-linejoin='round'/%3E%3Ccircle cx='36' cy='66' r='6' fill='%23F5A623'/%3E%3Ccircle cx='62' cy='66' r='6' fill='%23F5A623'/%3E%3C/svg%3E",
+  },
+};
+
+// Without this, mobile browsers fall back to a ~980px virtual viewport and
+// scale the desktop layout down instead of reflowing it, which makes every
+// max-width media query in globals.css unreliable on real phones.
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
+// Runs before hydration so the correct theme is applied on first paint —
+// avoids a flash of the wrong theme when the stored/system preference is dark.
+const THEME_INIT_SCRIPT = `(function(){try{var t=localStorage.getItem('haulwise-theme');if(!t){t=window.matchMedia('(prefers-color-scheme: dark)').matches?'dark':'light';}document.documentElement.setAttribute('data-theme',t);}catch(e){}})();`;
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html
+      lang="en"
+      className={`${inter.variable} ${manrope.variable} ${jetbrainsMono.variable}`}
+      suppressHydrationWarning
+    >
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: THEME_INIT_SCRIPT }} />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
