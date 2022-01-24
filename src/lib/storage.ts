cc2c17e2f7999067f458308257eeb4896d82a56f
HenokGuluma <htg218@nyu.edu>
2022-01-24 20:11:12 +0300

    Fix bug in storage

---
+++ src/lib/storage.ts
@@ -0,0 +1,132 @@
+// Storage abstraction for uploaded documents (BOL/POD/rate confirmations,
+// customer/driver files). Three implementations behind the same interface,
+// selected via STORAGE_DRIVER=local|vercel-blob|s3 (defaults to local):
+//
+// - local: writes to a gitignored ./uploads directory. Works for local dev
+//   and for the Docker Compose self-hosted deployment path (mount a volume
+//   at /app/uploads there for persistence across container restarts).
+//   Does NOT work on serverless platforms like Vercel — there is no
+//   persistent writable disk shared across function invocations.
+// - vercel-blob: real, functional — backed by a private Vercel Blob store
+//   (BLOB_READ_WRITE_TOKEN). This is what production uses.
+// - s3: structurally ready for any S3-compatible bucket (AWS S3, Cloudflare
+//   R2, Supabase Storage all speak the same REST API) but stubbed — it
+//   validates config and throws until real credentials are wired up and the
+//   signed PUT/GET/DELETE calls are implemented below. Swapping it in is a
+//   config + implementation change here only; no caller changes.
+//
+// Not imported with "server-only" because prisma/seed.ts (run via tsx, not
+// the Next.js bundler) also needs it.
+
+import fs from "node:fs/promises";
+import path from "node:path";
+import { put as blobPut, get as blobGet, del as blobDel } from "@vercel/blob";
+
+export type StoredFile = { key: string; sizeBytes: number; mimeType: string };
+
+export interface StorageDriver {
+  put(key: string, buffer: Buffer, mimeType: string): Promise<StoredFile>;
+  get(key: string): Promise<{ buffer: Buffer; mimeType: string } | null>;
+  delete(key: string): Promise<void>;
+}
+
+const UPLOAD_ROOT = path.join(process.cwd(), "uploads");
+
+class LocalDiskStorage implements StorageDriver {
+  async put(key: string, buffer: Buffer, mimeType: string): Promise<StoredFile> {
+    const filePath = path.join(UPLOAD_ROOT, key);
+    await fs.mkdir(path.dirname(filePath), { recursive: true });
+    await fs.writeFile(filePath, buffer);
+    await fs.writeFile(filePath + ".meta.json", JSON.stringify({ mimeType }));
+    return { key, sizeBytes: buffer.byteLength, mimeType };
+  }
+
+  async get(key: string): Promise<{ buffer: Buffer; mimeType: string } | null> {
+    const filePath = path.join(UPLOAD_ROOT, key);
+    try {
+      const buffer = await fs.readFile(filePath);
+      let mimeType = "application/octet-stream";
+      try {
+        mimeType = JSON.parse(await fs.readFile(filePath + ".meta.json", "utf-8")).mimeType ?? mimeType;
+      } catch {
+        // missing/corrupt sidecar — fall back to the generic type
+      }
+      return { buffer, mimeType };
+    } catch {
+      return null;
+    }
+  }
+
+  async delete(key: string): Promise<void> {
+    const filePath = path.join(UPLOAD_ROOT, key);
+    await fs.unlink(filePath).catch(() => {});
+    await fs.unlink(filePath + ".meta.json").catch(() => {});
+  }
+}
+
+class VercelBlobStorage implements StorageDriver {
+  async put(key: string, buffer: Buffer, mimeType: string): Promise<StoredFile> {
+    await blobPut(key, buffer, { access: "private", contentType: mimeType, addRandomSuffix: false });
+    return { key, sizeBytes: buffer.byteLength, mimeType };
+  }
+
+  async get(key: string): Promise<{ buffer: Buffer; mimeType: string } | null> {
+    try {
+      const result = await blobGet(key, { access: "private" });
+      if (!result || !result.stream) return null;
+      const reader = result.stream.getReader();
+      const chunks: Buffer[] = [];
+      for (;;) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        if (value) chunks.push(Buffer.from(value));
+      }
+      return { buffer: Buffer.concat(chunks), mimeType: result.blob.contentType || "application/octet-stream" };
+    } catch {
+      return null;
+    }
+  }
+
+  async delete(key: string): Promise<void> {
+    await blobDel(key).catch(() => {});
+  }
+}
+
+class StubS3Storage implements StorageDriver {
+  private requireConfig() {
+    if (!process.env.S3_BUCKET) {
+      throw new Error(
+        "STORAGE_DRIVER=s3 but S3_BUCKET is not set. Configure S3_BUCKET, S3_ENDPOINT, S3_REGION, " +
+          "S3_ACCESS_KEY_ID, and S3_SECRET_ACCESS_KEY, then implement the signed PUT/GET/DELETE " +
+          "requests in src/lib/storage.ts (StubS3Storage) — the StorageDriver interface and every " +
+          "caller already treat this the same as the local driver."
+      );
+    }
+  }
+  async put(): Promise<StoredFile> {
+    this.requireConfig();
+    throw new Error("S3 storage driver is a stub — see the comment in src/lib/storage.ts.");
+  }
+  async get(): Promise<{ buffer: Buffer; mimeType: string } | null> {
+    this.requireConfig();
+    throw new Error("S3 storage driver is a stub — see the comment in src/lib/storage.ts.");
+  }
+  async delete(): Promise<void> {
+    this.requireConfig();
+    throw new Error("S3 storage driver is a stub — see the comment in src/lib/storage.ts.");
+  }
+}
+
+let driver: StorageDriver | null = null;
+
+export function getStorageDriver(): StorageDriver {
+  if (!driver) {
+    if (process.env.STORAGE_DRIVER === "s3") driver = new StubS3Storage();
+    else if (process.env.STORAGE_DRIVER === "vercel-blob") driver = new VercelBlobStorage();
+    else driver = new LocalDiskStorage();
+  }
+  return driver;
+}
+
+export const ALLOWED_DOCUMENT_MIME_TYPES = new Set(["application/pdf", "image/png", "image/jpeg"]);
+export const MAX_DOCUMENT_SIZE_BYTES = 15 * 1024 * 1024; // 15MB
