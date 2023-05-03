721af0ded584304625de410a0285e6e68e8b97a3
HenokGuluma <htg218@nyu.edu>
2023-05-03 16:32:43 +0300

    Add route module

---
+++ src/app/api/loads/[id]/activity/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { requireUser } from "@/lib/auth";
+import { fullName } from "@/lib/format";
+
+/** Combined audit trail + comments for a load's detail-drawer timeline. */
+export async function GET(_req: NextRequest, { params }: { params: { id: string } }) {
+  const auth = await requireUser();
+  if ("error" in auth) return NextResponse.json({ error: auth.error }, { status: auth.status });
+
+  const [activities, comments] = await Promise.all([
+    prisma.loadActivity.findMany({
+      where: { loadId: params.id },
+      orderBy: { createdAt: "desc" },
+      include: { actorUser: { select: { id: true, firstName: true, lastName: true } } },
+    }),
+    prisma.loadComment.findMany({
+      where: { loadId: params.id },
+      orderBy: { createdAt: "desc" },
+      include: { authorUser: { select: { id: true, firstName: true, lastName: true } } },
+    }),
+  ]);
+
+  return NextResponse.json({
+    activities: activities.map((a) => ({
+      ...a,
+      actorUser: a.actorUser ? { id: a.actorUser.id, name: fullName(a.actorUser.firstName, a.actorUser.lastName) } : null,
+    })),
+    comments: comments.map((c) => ({
+      ...c,
+      authorUser: { id: c.authorUser.id, name: fullName(c.authorUser.firstName, c.authorUser.lastName) },
+    })),
+  });
+}
