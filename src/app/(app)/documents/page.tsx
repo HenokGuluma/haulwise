01c31731c4aebff03f4f120dda89f7d8635a53bd
HenokGuluma <htg218@nyu.edu>
2023-09-12 09:39:41 +0300

    Fix bug in page

---
+++ src/app/(app)/documents/page.tsx
@@ -0,0 +1,30 @@
+import { prisma } from "@/lib/prisma";
+import { getSessionUser } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { DocumentsView } from "@/components/DocumentsView";
+import { demoScope } from "@/lib/demo-scope";
+
+export default async function DocumentsPage() {
+  const user = await getSessionUser();
+  if (!user) redirect("/login");
+
+  const scope = demoScope(user);
+  const [payAgg, pendingAgg, withDocs, loadCount] = await Promise.all([
+    prisma.load.aggregate({ where: { ...scope, payoutStatus: { not: "NOT_BILLED" } }, _sum: { driverPay: true } }),
+    prisma.load.aggregate({ where: { ...scope, payoutStatus: "PENDING" }, _sum: { driverPay: true } }),
+    prisma.load.count({ where: { ...scope, documents: { some: {} } } }),
+    prisma.load.count({ where: scope }),
+  ]);
+
+  return (
+    <DocumentsView
+      user={user}
+      totals={{
+        totalPay: payAgg._sum.driverPay ?? 0,
+        totalPending: pendingAgg._sum.driverPay ?? 0,
+        withDocs,
+        loadCount,
+      }}
+    />
+  );
+}
